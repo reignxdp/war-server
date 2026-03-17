319eb9e0d99348ca5c0ea7f563f80d883d6231fb
reignxdp <emirefedemon@gmail.com>
2026-03-17 21:15:07 +0300

    Refactor socket connection and clean up code
    Updated socket connection to use the same domain and removed console log for player data.

--- client/game.js
+++ client/game.js
@@ -1,9 +1,7 @@
-// Render server linki burada
-const socket = io("https://war-server-5u9e.onrender.com"); 
+const socket = io(); // Aynı domain üzerinden bağlanıyor
 
 let players = {};
 
-// Phaser oyun config
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -19,7 +17,6 @@ const game = new Phaser.Game(config);
 function create() {}
 
 function update() {
-  // Tıklayınca oyuncu koordinatlarını gönder
   if (this.input.activePointer.isDown) {
     socket.emit("move", {
       x: this.input.x,
@@ -27,7 +24,6 @@ function update() {
     });
   }
 
-  // Basit çizim
   const ctx = game.canvas.getContext("2d");
   ctx.clearRect(0, 0, game.config.width, game.config.height);
   ctx.fillStyle = "red";
@@ -38,9 +34,6 @@ function update() {
   }
 }
 
-// Serverdan oyuncu verisi al
 socket.on("updatePlayers", (data) => {
   players = data;
-  // Konsolda test edebilirsin
-  console.log(players);
 });
