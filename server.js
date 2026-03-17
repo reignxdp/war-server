c17d4ed2b77d26c68af1fa9cb7c335919dea78de
reignxdp <emirefedemon@gmail.com>
2026-03-17 20:59:22 +0300

    Refactor server.js for improved readability and functionality

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -5,22 +6,30 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
+// WebSocket server, tüm domainlere izin (Vercel + mobil vs)
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*",
+  },
 });
 
+// Oyun durumunu saklayacağız
 let players = {};
 
+// Oyuncu bağlanınca
 io.on("connection", (socket) => {
-  console.log("baglandi:", socket.id);
+  console.log("Oyuncu bağlandı:", socket.id);
 
+  // Rastgele başlangıç koordinatları
   players[socket.id] = {
     x: Math.random() * 500,
-    y: Math.random() * 500
+    y: Math.random() * 500,
   };
 
+  // Tüm oyunculara güncel oyuncu listesi gönder
   io.emit("updatePlayers", players);
 
+  // Oyuncu hareket edince
   socket.on("move", (data) => {
     if (players[socket.id]) {
       players[socket.id].x = data.x;
@@ -29,12 +38,20 @@ io.on("connection", (socket) => {
     io.emit("updatePlayers", players);
   });
 
+  // Oyuncu disconnect olunca
   socket.on("disconnect", () => {
     delete players[socket.id];
     io.emit("updatePlayers", players);
   });
 });
 
-server.listen(3000, () => {
-  console.log("server calisiyor");
+// Opsiyonel: / açınca basit mesaj
+app.get("/", (req, res) => {
+  res.send("Server çalışıyor!");
+});
+
+// Server portu (Render bunu otomatik yönlendiriyor)
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server ${PORT} portunda çalışıyor`);
 });
