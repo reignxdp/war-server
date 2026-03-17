32443a7b771397cfa8b4ec8cbde42cef2eb94dff
reignxdp <emirefedemon@gmail.com>
2026-03-17 21:27:27 +0300

    Refactor game.js for consistent formatting

--- client/game.js
+++ client/game.js
@@ -1,4 +1,4 @@
-const socket = io(); // Aynı domain üzerinden bağlanıyor
+const socket = io(); // Aynı domain
 
 let players = {};
 
@@ -7,33 +7,28 @@ const config = {
   width: 800,
   height: 600,
   scene: {
-    create: create,
-    update: update
+    create:create,
+    update:update
   }
 };
 
 const game = new Phaser.Game(config);
 
-function create() {}
+function create(){}
 
-function update() {
-  if (this.input.activePointer.isDown) {
-    socket.emit("move", {
-      x: this.input.x,
-      y: this.input.y
-    });
+function update(){
+  if(this.input.activePointer.isDown){
+    socket.emit("move", { x:this.input.x, y:this.input.y });
   }
 
   const ctx = game.canvas.getContext("2d");
-  ctx.clearRect(0, 0, game.config.width, game.config.height);
+  ctx.clearRect(0,0,game.config.width,game.config.height);
   ctx.fillStyle = "red";
 
-  for (let id in players) {
-    let p = players[id];
-    ctx.fillRect(p.x, p.y, 20, 20);
+  for(let id in players){
+    const p = players[id];
+    ctx.fillRect(p.x,p.y,20,20);
   }
 }
 
-socket.on("updatePlayers", (data) => {
-  players = data;
-});
+socket.on("updatePlayers", (data)=>{ players = data; });
